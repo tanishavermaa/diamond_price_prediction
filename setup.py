7165153d37e012c183b9293375d7310e0856da80
tanishavermaa <vtanishaaa@gmail.com>
2023-11-20 22:40:05 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n"," ") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+    
+    
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Tanisha',
+    author_email='vtanishaaa@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
